010136c2434bb9cac02c3c1922e3f92f3cc3dcbd
anusuyagovindarajan <anusuya.govindarajan@gmail.com>
2017-08-25 04:52:23 +0530

    v.1.1

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,17 +1,23 @@
 package com.company;
 
-abstract public class Fruit {
+public class Item {
 
     String name;
+    String category;
     double price;
 
-    public Fruit(String name , double price) {
+    public Item(String name, String category, double price) {
         this.name = name;
+        this.category = category;
         this.price = price;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public String getCategory() {
+        return this.category;
     }
 
     public double getPrice() {
